dfa8321980fb975409950671d4daa666a17400c6
Silambarasan <msilambarasan98@gmail.com>
2023-09-04 15:47:29 +0530

    first commit

---
+++ P-17/P-17/Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using P_17.Models;
+
+namespace P_17.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<P_17.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ P-17/P-17/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace P_17.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+    }
+}
